86fc23bfc7ba3045f966dcf5486a0f960c770fee
Rodrigo Haro <68792784+RodraHa@users.noreply.github.com>
2025-08-29 08:45:10 -0500

    Primer commit

---
+++ src/test/java/com/nttdata/stepsdefinitions/CarritoStepsDef.java
@@ -0,0 +1,33 @@
+package com.nttdata.stepsdefinitions;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CarritoStepsDef {
+    @Given("estoy en la aplicación de SauceLabs")
+    public void estoyEnLaAplicaciónDeSauceLabs() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("valido que carguen correctamente los productos en la galeria")
+    public void validoQueCarguenCorrectamenteLosProductosEnLaGaleria() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("valido el carrito de compra actualice correctamente")
+    public void validoElCarritoDeCompraActualiceCorrectamente() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("agrego {string} del siguiente producto {string}")
+    public void agregoDelSiguienteProducto(String arg0, String arg1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
